f60585d62b2ad72442b89ea32948dde0d08258ed
Kato <kubkate@gmail.com>
2020-12-17 17:26:48 -0800

    fetch mock data

--- src/pages/Library/Library.js
+++ src/pages/Library/Library.js
@@ -1,19 +1,67 @@
+import { useEffect, useState } from 'react';
 import './Library.css';
 
-
-// pseudo database
-const data = {
-  data: [
-    {
-      id: '1',
-      firstName: 'Robin',
-      lastName: 'Wieruch',
-      isDeveloper: true,
-    },
-  ]
-};
+const mockLibraries = [
+  {
+    "id": 1,
+    "first_name": "John",
+    "last_name": "Doe",
+    "email": "johndoe@abc.com",
+    "gender": "Male",
+    "status": "Terminated"
+  },
+  {
+    "id": 2,
+    "first_name": "Jane",
+    "last_name": "Doe",
+    "email": "janedoe@abc.com",
+    "gender": "Female",
+    "status": "New"
+  },
+  {
+    "id": 3,
+    "first_name": "Alice",
+    "last_name": "Doe",
+    "email": "alicedoe@abc.com",
+    "gender": "Female",
+    "status": "Leaving"
+  },
+  {
+    "id": 4,
+    "first_name": "Bob",
+    "last_name": "Doe",
+    "email": "bobdoe@abc.com",
+     "gender": "Male",
+    "status": "Active"
+  }
+]
 
 const LibraryPage = () => {
+  const [libraries, setLibraries] = useState([]);
+  
+  /**
+    * Fetch Data
+    */
+  // const libraryURL = "../../assets/fakedb.json";
+  // useEffect(() => {
+  //   // Use IFFE to handle asynchronous in useEffect 
+  //   (async () => {
+  //     try {
+  //       const response = await fetch(libraryURL);
+  //       const data = await response.json()
+
+  //       setLibraries(data);
+
+  //     } catch (error) {
+  //       console.log("IN FETCHLIBRARY ERROR: ", error)
+  //    // return error.message
+  //     }
+  //   })()
+  // },[])
+
+  useEffect(() => {
+    setLibraries(mockLibraries);
+  },[])
 
   return (
     <div>
