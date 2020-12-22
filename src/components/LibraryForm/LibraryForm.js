b95cc00060bf6a49f60b0d8ba0ea2067c6f8981b
Kato <kubkate@gmail.com>
2020-12-21 19:47:55 -0800

    category selections in form

--- src/components/LibraryForm/LibraryForm.js
+++ src/components/LibraryForm/LibraryForm.js
@@ -4,16 +4,23 @@ const LibraryForm = () => {
   return (
     <Container>
        <Form>
-        <Form.Group controlId="formCategory">
-          <Form.Label>Category</Form.Label>
-          <Form.Control type="text" placeholder="Category"></Form.Control>
+        <Form.Group controlId="form.ControlCategorySelect">
+          <Form.Label>Select Category</Form.Label>
+          <Form.Control as="select">
+            <option value="0">Choose...</option>
+            <option value="1">One</option>
+            <option value="2">Two</option>
+            <option value="3">Three</option>
+          </Form.Control>
         </Form.Group>
           <Form.Label>Sub Category</Form.Label>
           <Form.Control type="text" placeholder="Sub-Category"></Form.Control>
           <Form.Label>Title</Form.Label>
           <Form.Control type="text" placeholder="Title"></Form.Control>
+        <Form.Group controlId="form.ControlNoteTextarea">
           <Form.Label>Note</Form.Label>
-          <Form.Control type="text" placeholder="Note"></Form.Control>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
           <Form.Label>Link URL</Form.Label>
           <Form.Control type="text" placeholder="Link URL"></Form.Control>
           <Form.Label>Image URL</Form.Label>
